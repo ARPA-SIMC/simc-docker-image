8eee2ad4064205d2ca1244347a80c2d5b5e68f17
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2018-10-26 14:06:57 +0200

    CentOS support in build script

--- build.sh
+++ build.sh
@@ -3,11 +3,33 @@ set -uex
 
 source "$1"
 
-sed -i '/^tsflags=/d' /etc/dnf/dnf.conf
-dnf install -y @buildsys-build
-dnf install -y 'dnf-command(builddep)'
-dnf install -y git
-dnf copr enable -y $COPR
+distro="$2"
+
+if [[ "$distro" == centos ]]
+then
+    sed -i '/^tsflags=/d' /etc/yum.conf
+    yum install -y epel-release
+    yum install -y @buildsys-build
+    yum install -y yum-utils
+    yum install -y yum-plugin-copr
+    yum install -y git
+    for copr in $COPR
+    do
+        yum copr enable -y $copr epel-7
+    done
+elif [[ "$distro" == fedora ]]
+    sed -i '/^tsflags=/d' /etc/dnf/dnf.conf
+    dnf install -y @buildsys-build
+    dnf install -y 'dnf-command(builddep)'
+    dnf install -y git
+    for copr in $COPR
+    do
+        dnf copr enable -y $copr
+    done
+else
+    echo "$distro is not a supported Linux distribution" >&2
+    exit 1
+fi
 
 mkdir src
 pushd src
